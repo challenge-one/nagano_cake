75cd392069053cc278da625db16840ba25f883ea
taaachezi <taaachezi@gmail.com>
2020-08-17 14:45:00 +0000

    controller,routes作成

---
+++ app/controllers/public/deliveries_controller.rb
@@ -0,0 +1,16 @@
+class Public::DeliveriesController < ApplicationController
+  def create
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,39 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions',
+    passwords: 'customers/passwords'
+  }
+
+  namespace :admin do
+    resources :products
+    resources :genres, except: [:new,:show,:destroy]
+    resources :order_items, only: :update
+    resources :orders, only: [:show,:index,:update]
+    resources :customers, except: [:new,:create,:destroy]
+  end
+
+  namespace :public do
+    resource :customers, only: [:show,:edit,:update]
+    resources :products, only: [:show,:index]
+    resources :deliveries, except: [:new,:show]
+    resources :orders, except: [:update,:destroy,:edit]
+    resources :cart_items, except: [:new,:edit,:show]
+  end
+
+  get '/' => 'public/homes#top'
+  get 'homes/about' => 'public/homes#about'
+  get '/admin' => 'admin/homes#top'
+  get 'cart_items/destroy_all' => 'public/cart_items#destroy_all'
+  get 'customers/unsubscribe' => 'public/customers#unsubscribe'
+  get 'customers/withdraw' => 'public/customers#withdraw'
+  get 'orders/confirm' => 'public/orders#confirm'
+  get 'orders/complete' => 'public/orders#complete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
