c5bba28ee25d008e419e4af99211c0181732eb8d
yoichihashimoto <yoichi3223@gmail.com>
2020-08-20 13:43:33 +0900

    product add

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,22 +1,35 @@
 class Admin::ProductsController < ApplicationController
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to admin_product_path(@product)
   end
-
   def edit
+    @product = Product.find(params[:id])
   end
 
   def show
+    @product = Product.find(params[:id])
+    @genre = @product.genre
   end
 
   def index
+    @products = Product.all
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to admin_product_path(@product)
   end
 
   def destroy
+  end
+def product_params
+    params.require(:product).permit(:name,:introduction,:price,:status,:image,:genre_id)
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,7 +1,8 @@
 class Genre < ApplicationRecord
+	has_many :products, dependent: :destroy
 	validates :name, uniqueness: true
 	enum status: {
-		true: 0,
-		false: 1
+		 true: 0,
+		 false: 1
 	},_prefix: true
 end
