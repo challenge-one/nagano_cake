3f589aa8ab2bcbacfa3138a42ab50a7dbe484e00
yoichihashimoto <yoichi3223@gmail.com>
2020-08-21 22:43:37 +0900

    modifiled product2

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,13 @@
 class Admin::OrdersController < ApplicationController
 	
   def index
+  	# ヘッダーからの情報取得（全履歴）
+  	@orders = Order.all
+  	#  会員詳細からの情報取得（該当会員）
+  	@customer_orders = Customer.orders.all
+  	#  トップページからの情報取得（本日）
+  	#  whereで範囲指定しただけなので取得できていない
+  	@today_orders = Order.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
   end
 
   def show
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,19 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :family_name, presence: true
+  validates :family_rubi, presence: true
+  validates :first_name, presence: true
+  validates :first_rubi, presence: true
+  validates :postcode, presence: true
+  validates :address, presence: true
+  validates :tell, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
+
+   enum status: {
+   	true: 0,
+   	false: 1
+   },_prefix: true
 end
+
