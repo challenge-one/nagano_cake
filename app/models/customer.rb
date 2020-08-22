1a90a14895ff4c34b947cacdd80632527517979e
vagrant <k.taki415@gmail.com>
2020-08-22 04:16:14 +0000

    [Add] orders_view

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,12 +4,6 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-<<<<<<< HEAD
-  enum status:{有効: 0,退会: 1}
-  def active_for_authentication?
-    super && (self.status == "有効")
-  end
-=======
   validates :family_name, presence: true
   validates :family_rubi, presence: true
   validates :first_name, presence: true
@@ -20,9 +14,10 @@ class Customer < ApplicationRecord
   validates :email, uniqueness: true, presence: true
 
    enum status: {
-   	true: 0,
-   	false: 1
+    true: 0,
+    false: 1
    },_prefix: true
->>>>>>> a9ace58fd8f0a0dae1afc4a0abac86f37919b9b8
+
+   
 end
 
