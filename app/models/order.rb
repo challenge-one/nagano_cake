e0209e2e76a700222cc88dcb16154ac54eea2e42
taaachezi <taaachezi@gmail.com>
2020-08-23 14:07:25 +0000

    [add]search,pulic/order:view

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,13 @@ class Order < ApplicationRecord
   has_many :products, :through => :order_items
   accepts_nested_attributes_for :order_items
   belongs_to :customer
+  enum status: {
+		wait: 0,
+		confirmation: 1,
+		creating: 2,
+		set_delivery: 3,
+		deliveried: 4
+
+  }_prefix: true
+
 end
