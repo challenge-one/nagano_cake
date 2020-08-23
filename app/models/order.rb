2baa9c5b7ce4dfaabcb2f276dacf7f5322545e9c
taaachezi <taaachezi@gmail.com>
2020-08-23 13:53:57 +0000

    [add]search

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,10 @@
 class Order < ApplicationRecord
+	enum status: {
+		wait: 0,
+		confirmation: 1,
+		creating: 2,
+		set_delivery: 3,
+		deliveried: 4
+
+	}_prefix: true
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,2 +1,8 @@
 class OrderItem < ApplicationRecord
+	enum status: {
+		wait: 0,
+		create_wait: 1,
+		creating: 2,
+		created: 3
+	}_prefix: true
 end
