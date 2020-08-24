257311263415717ea739c554028050f427f97d8b
yoichi3223 <yoichi3223@gmail.com>
2020-08-24 09:15:17 +0000

    order modifiled3

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,104 +1,111 @@
 class Public::OrdersController < ApplicationController
-  before_action :authenticate_customer!
-  before_action :set_customer
+before_action :authenticate_customer!
+before_action :set_customer
 
-  def new
-    @order = Order.new
-  end
+    def new
+      @order = Order.new
+    end
 
-  def create
-    if current_customer.cart_items.exists?
-      @order = Order.new(order_params)
-      @order.customer_id = current_customer.id
-      @customer = current_customer
-      @add = params[:order][:add].to_i
+    def create
+      if current_customer.cart_items.exists?
+        @order = Order.new(order_params)
+        @order.customer_id = current_customer.id
+        @customer = current_customer
+        @order.cash_method = params[:order][:cash_method].to_i
+        @cart_items = current_customer.cart_items
+        total = 0
+        @cart_items.each do |cart_item|
+        total += cart_item.product.price * 1.10 * cart_item.number
+      end
+        @order.total_amount = total
+        @add = params[:order][:address].to_i
+
       case @add
-        when 1
-          @order.postcode = @customer.postcode
-          @order.address = @customer.address
-          @order.addressee = full_name(@customer)
-        when 2
-          @order.postcode = params[:order][:postcode]
-          @order.address = params[:order][:address]
-          @order.addressee = params[:order][:addressee]
-        when 3
-          @order.postcode = params[:order][:postcode]
-          @order.address = params[:order][:address]
-          @order.addressee = params[:order][:addressee]
+      when @add == 1
+        @order.postcode = @customer.postcode
+        @order.address = @customer.address
+        @order.addressee = @customer.addressee
+      when @add == 2
+        @order.postcode = params[:order][:postcode]
+        @order.address = params[:order][:address]
+        @order.addressee = params[:order][:addressee]
+      when @add == 3
+        address = Address.new(address_params)
+        address.customer_id = current_customer.id
+        address.save
+        @order.postcode = params[:order][:postcode]
+        @order.address = params[:order][:address]
+        @order.addressee = params[:order][:addressee]
       end
-      @order.save
-      if Delivery.find_by(address: @order.address).nil?
-        @address = Delivery.new
-        @address.postcode = @order.postcode
-        @address.address = @order.address
-        @address.addressee = @order.addressee
-        @address.customer_id = current_customer.id
-        @address.save
-      end
+        @order.save
 
-      current_customer.cart_items.each do |cart_item|
+        current_customer.cart_items.each do |cart_item|
         order_item = @order.order_items.build
         order_item.order_id = @order.id
         order_item.product_id = cart_item.product_id
         order_item.number = cart_item.number
-        order_item.purchased_price = cart_item.product.purchased_price
+        order_item.purchased_price = cart_item.product.price
         order_item.save
         cart_item.destroy
       end
 
-      render :complete
-    else
-       redirect_to root_path
-     end
-   end
-
-
-  def index
-    @orders = @customer.orders
-  end
-
-  def show
-    @order = Order.find(params[:id])
-    if @order.customer_id != current_customer.id
-      redirect_back(fallback_location: root_path)
-      flash[:notice] = "アクセスに失敗しました"
+        render :complete
+    
+      else
+        redirect_to root_path
+      end
     end
-  end
 
-  def confirm
-      @order = Order.new
-      @cart_item = current_customer.cart_items
-      @order.cash_method = params[:cash_method]
 
-      @add = params[:add].to_i
-      case @add
-      when 1
-        @order.postcode = @customer.postcode
-        @order.address = @customer.address
-        @order.addressee = @customer.family_name
-      when 2
-        @sta = params[:order][:address].to_i
+      def index
+        @orders = @customer.orders
+      end
+
+      def show
+        @order = Order.find(params[:id])
+      if @order.customer_id != current_customer.id
+        redirect_back(fallback_location: root_path)
+        flash[:notice] = "アクセスに失敗しました"
+        end
+      end
+
+    def confirm
+        @order = Order.new
+        @order.cash_method = params[:order][:cash_method]
+        @cart_items = current_customer.cart_items
+        @add = params[:order][:@add].to_i
+      if @add.to_i == 1
+        @order.postcode = current_customer.postcode
+        @order.address = current_customer.address
+        @order.addressee = current_customer.family_name
+      elsif @add.to_i == 2
+        @sta = params[:order][:id].to_i
         @address = Delivery.find(@sta)
-        @order.postcode =@address.address
+        @order.postcode = @address.address
         @order.addressee = @address.addressee
-      when 3
+      elsif @add.to_i == 3
         @order.postcode = params[:order][:postcode]
         @order.address = params[:order][:address]
         @order.addressee = params[:order][:addressee]
       end
     end
 
-  def complete
-  end
+    def complete
+    end
 
-  private
-  def set_customer
+    private
+    def set_customer
     @customer = current_customer
-  end
-  def order_params
-    params.require(:order).permit(
-      :created_at, :address, :addressee, :status, :cash_method, :postcode, :postage,
-      order_items_attributes: [:order_id, :product_id, :number, :purchased_price, :status]
-      )
-  end
-end
+    end
+
+    def order_params
+      params.require(:order).permit(
+      :total_amount,:created_at, :address, :addressee, :status, :cash_method, :postcode, :postage,
+      order_items_attributes: [:order_id, :product_id, :number, :purchased_price, :status])
+    end
+
+    def address_params
+      params.require(:address).permit(
+      :customer_id, :postcode, :address, :addressee)
+      end
+    end
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,5 @@ class Order < ApplicationRecord
   has_many :products, :through => :order_items
   accepts_nested_attributes_for :order_items
   belongs_to :customer
+  enum cash_method: {クレジットカード:0, 銀行振込:1}
 end
