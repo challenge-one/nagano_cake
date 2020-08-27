56362f692d13c043765e24511e45934b28f2a7d9
vagrant <k.taki415@gmail.com>
2020-08-27 04:54:34 +0000

    [Fix] admin/status

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -27,7 +27,9 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     if @order.update(order_params)
       flash[:success] = "注文ステータスを変更しました"
-      @order.order_cash_complete
+      if @order.status.include?("入金確認")
+        @order.order_cash_complete
+      end
       redirect_to admin_order_path(@order)
     else
       render "show"
